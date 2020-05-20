b35e3548e141ee2a8d72ef0e8f1bfee9126d2c68
mirse-partner <879937147@qq.com>
2020-05-20 21:21:30 +0800

    完善2层级recyclerview

--- app/src/main/java/com/wdz/studycollection/recyclerview/updownrecyclerview/UpDownAdapter.java
+++ app/src/main/java/com/wdz/studycollection/recyclerview/updownrecyclerview/UpDownAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.wdz.studycollection.R;
@@ -14,74 +15,73 @@ import com.wdz.studycollection.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UpDownAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    private List<UpDownRecyclerViewActivity.Device> mList = new ArrayList<>();
+public class UpDownAdapter extends RecyclerView.Adapter<UpDownAdapter.ViewHolder> {
+    private UpDownRecyclerViewActivity.Device mDevice;
     private Context context;
     private final int TYPE_BULB = 0;
     private final int TYPE_GATEWAY = 1;
 
 
 
-    public static enum ITEM_TYPE{
-        ITEM_TYPE_BULB,
-        ITEM_TYPE_GATEWAY
-    }
-
-    public UpDownAdapter(List<UpDownRecyclerViewActivity.Device> deviceList, Context context) {
-        mList = deviceList;
+    public UpDownAdapter(UpDownRecyclerViewActivity.Device device, Context context) {
+        mDevice = device;
         this.context = context;
     }
 
-    public void addDevice(UpDownRecyclerViewActivity.Device device) {
-        mList.add(device);
+    public void updateDevice(UpDownRecyclerViewActivity.Device device) {
+        mDevice = device;
         notifyDataSetChanged();
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        switch (viewType){
-            case TYPE_BULB:
-                return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_bulb,parent,false));
-            case TYPE_GATEWAY:
-                return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_gateway,parent,false));
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_home_content,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UpDownAdapter.ViewHolder holder, int position) {
+        switch (position){
+            case 0:
+                List<UpDownRecyclerViewActivity.Bulb> bulbList = mDevice.getBulbList();
+                holder.mTextView.setText("灯");
+                holder.mRecyclerView.setHasFixedSize(true);
+                holder.mRecyclerView.setLayoutManager(new GridLayoutManager(context, 3,
+                        GridLayoutManager.VERTICAL, false));
+                ItemRecyclerViewAdapter mItemRecyclerViewAdapter = new ItemRecyclerViewAdapter(position);
+                holder.mRecyclerView.setAdapter(mItemRecyclerViewAdapter);
+                break;
+            case 1:
+                List<UpDownRecyclerViewActivity.Gateway> gatewayList = mDevice.getGatewayList();
+                holder.mTextView.setText("网关");
+                holder.mRecyclerView.setHasFixedSize(true);
+                holder.mRecyclerView.setLayoutManager(new GridLayoutManager(context, 3,
+                        GridLayoutManager.VERTICAL, false));
+                ItemRecyclerViewAdapter mItemRecyclerViewAdapter1 = new ItemRecyclerViewAdapter(gatewayList);
+                holder.mRecyclerView.setAdapter(mItemRecyclerViewAdapter1);
+                break;
             default:
                 break;
         }
-        return null;
-    }
 
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
 
     }
 
-    @Override
-    public int getItemViewType(int position) {
-        UpDownRecyclerViewActivity.Device device = mList.get(position);
-        if (device.deviceType == TYPE_BULB){
-            return TYPE_BULB;
-        }
-        else if (device.deviceType == TYPE_GATEWAY){
-            return TYPE_GATEWAY;
-        }
-        else{
-            return TYPE_BULB;
-        }
-
-    }
 
     @Override
     public int getItemCount() {
-        return mList == null?0:mList.size();
+        return 2;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
 
-        TextView mTvItem;
+        TextView mTextView;
+        RecyclerView mRecyclerView;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            mTextView = itemView.findViewById(R.id.tv_recycler_name);
+            mRecyclerView = itemView.findViewById(R.id.tv_recycler_view);
         }
     }
 }
--- app/src/main/java/com/wdz/studycollection/recyclerview/updownrecyclerview/UpDownRecyclerViewActivity.java
+++ app/src/main/java/com/wdz/studycollection/recyclerview/updownrecyclerview/UpDownRecyclerViewActivity.java
@@ -2,6 +2,7 @@ package com.wdz.studycollection.recyclerview.updownrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -21,27 +22,33 @@ public class UpDownRecyclerViewActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     private List<Device> mList = new ArrayList<>();
     private UpDownAdapter upDownAdapter;
+    private Device device;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_up_down_recycler_view);
         ButterKnife.bind(this);
-
-        upDownAdapter = new UpDownAdapter(mList, this);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, RecyclerView.VERTICAL, false);
-        recyclerView.setLayoutManager(gridLayoutManager);
+        device = new Device();
+        upDownAdapter = new UpDownAdapter(device, this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,  RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(upDownAdapter);
+
     }
 
     @OnClick({R.id.bt_add_bulb,R.id.bt_add_gateway})
     public void onClick(View view){
         switch (view.getId()){
             case R.id.bt_add_bulb:
-                upDownAdapter.addDevice(new Device(0));
+                Bulb bulb = new Bulb();
+                device.getBulbList().add(bulb);
+                upDownAdapter.updateDevice(device);
                 break;
             case R.id.bt_add_gateway:
-                upDownAdapter.addDevice(new Device(1));
+                Gateway gateway = new Gateway();
+                device.getGatewayList().add(gateway);
+                upDownAdapter.updateDevice(device);
                 break;
             default:
                 break;
@@ -51,9 +58,31 @@ public class UpDownRecyclerViewActivity extends AppCompatActivity {
 
     class Device{
         public int deviceType;
+        public List<Bulb> bulbList = new ArrayList<>();
+        public List<Gateway> gatewayList = new ArrayList<>();
 
-        public Device(int deviceType) {
-            this.deviceType = deviceType;
+        public List<Bulb> getBulbList() {
+            return bulbList;
         }
+
+        public void setBulbList(List<Bulb> bulbList) {
+            this.bulbList = bulbList;
+        }
+
+        public List<Gateway> getGatewayList() {
+            return gatewayList;
+        }
+
+        public void setGatewayList(List<Gateway> gatewayList) {
+            this.gatewayList = gatewayList;
+        }
+
+    }
+
+    class Bulb{
+
+    }
+    class Gateway{
+
     }
 }
