80948967262c785d277e7bddf65c9af94ab851df
dezhi.wang <153260719@qq.com>
2020-06-13 17:25:23 +0800

    初用ViewStub

--- app/src/main/java/com/wdz/studycollection/recyclerview/snaphelper/SnapAdapter.java
+++ app/src/main/java/com/wdz/studycollection/recyclerview/snaphelper/SnapAdapter.java
@@ -1,16 +1,32 @@
 package com.wdz.studycollection.recyclerview.snaphelper;
 
+import android.util.Log;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.wdz.studycollection.R;
 import com.wdz.studycollection.recyclerview.universal.BaseAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class SnapAdapter extends BaseAdapter<String> {
-    public SnapAdapter(List datas) {
+public class SnapAdapter extends BaseAdapter<SnapHelperActivity.Page> {
+    private static final String TAG = "SnapAdapter";
+    List<SnapHelperActivity.Page> datas = new ArrayList<>();
+    private OnClickListener onClickListener;
+    public SnapAdapter(List<SnapHelperActivity.Page> datas) {
         super(datas);
+        this.datas = datas;
+        //Log.i(TAG, "SnapAdapter: "+datas.toString());
+    }
+
+    public void refreshStatus(List<SnapHelperActivity.Page> datas){
+        this.datas = datas;
+        notifyDataSetChanged();
     }
 
     @Override
@@ -19,9 +35,50 @@ public class SnapAdapter extends BaseAdapter<String> {
     }
 
     @Override
-    public void convert(VH vh, String data, int position) {
+    public void convert(VH vh, SnapHelperActivity.Page data, int position) {
+        List<View> views = new ArrayList<>();
+        views.add(vh.getView(R.id.stub1));
+        views.add(vh.getView(R.id.stub2));
+        views.add(vh.getView(R.id.stub3));
+        views.add(vh.getView(R.id.stub4));
+        views.add(vh.getView(R.id.stub5));
+        views.add(vh.getView(R.id.stub6));
+        views.add(vh.getView(R.id.stub7));
+        views.add(vh.getView(R.id.stub8));
+
+
+        for (int i = 0; i <data.itemList.size() ; i++) {
+            int i1 = i;
+            ViewStub view = (ViewStub) views.get(i);
+            if (view!=null){
+
+            }
+            View view1 = view.inflate();
+            TextView tv = view1.findViewById(R.id.tv);
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (onClickListener!=null){
+                        onClickListener.onClick((8*position+i1));
+                    }
+
+                }
+            });
+            Log.i(TAG, "convert: "+data.itemList.toString());
+            tv.setText(data.itemList.get(i).getName());
+        }
+
 
     }
+    public interface OnClickListener{
+        void onClick(int position);
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener){
+        this.onClickListener = onClickListener;
+    }
+
+
 
 
 
--- app/src/main/java/com/wdz/studycollection/recyclerview/snaphelper/SnapHelperActivity.java
+++ app/src/main/java/com/wdz/studycollection/recyclerview/snaphelper/SnapHelperActivity.java
@@ -1,11 +1,14 @@
 package com.wdz.studycollection.recyclerview.snaphelper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.OrientationHelper;
 import androidx.recyclerview.widget.PagerSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.wdz.studycollection.R;
 
@@ -16,9 +19,10 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SnapHelperActivity extends AppCompatActivity {
+    private static final String TAG = "SnapHelperActivity";
     @BindView(R.id.recyclerView)
     RecyclerView recyclerView;
-    private List<String> mList = new ArrayList<>();
+    private List<Item> mList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +30,107 @@ public class SnapHelperActivity extends AppCompatActivity {
         setContentView(R.layout.activity_snap_helper);
         ButterKnife.bind(this);
         initData();
-        SnapAdapter snapAdapter = new SnapAdapter(mList);
+        resetData(mList);
+        SnapAdapter snapAdapter = new SnapAdapter(mPageList);
+
+//        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 4);
+//        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, RecyclerView.HORIZONTAL, false);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(snapAdapter);
         PagerSnapHelper snapHelper = new PagerSnapHelper();
         snapHelper.attachToRecyclerView(recyclerView);
+        snapAdapter.setOnClickListener(new SnapAdapter.OnClickListener() {
+            @Override
+            public void onClick(int position) {
+                for (Item item:mList) {
+                    item.setSelect(false);
+                }
+                mList.get(position).setSelect(true);
+                resetData(mList);
+                snapAdapter.refreshStatus(mPageList);
+                Log.i(TAG, "onClick: "+position);
+            }
+        });
+    }
+
+    private List<Page> mPageList = new ArrayList<>();
+    private void resetData(List<Item> mList) {
+        mPageList.clear();
+        int page = mList.size()/8;
+        if (mList.size()%8>0){
+            page++;
+        }
+        for (int i=0;i<page;i++){
+
+            int fromIndex = i*8;
+            int toIndex = (i+1)*8;
+            if (toIndex>mList.size()){
+                toIndex = mList.size();
+            }
+            Page page1 = new Page(new ArrayList<>(mList.subList(fromIndex,toIndex)));
+            mPageList.add(page1);
+        }
     }
 
     private void initData() {
         for (int i=0;i<20;i++){
-            mList.add(String.valueOf(i));
+            mList.add(new Item(String.valueOf(i),false));
+        }
+    }
+    public class Page{
+        public Page(List<Item> itemList) {
+            this.itemList = itemList;
+        }
+
+        public List<Item> itemList;
+
+        public List<Item> getItemList() {
+            return itemList;
+        }
+
+        public void setItemList(List<Item> itemList) {
+            this.itemList = itemList;
+        }
+
+        @Override
+        public String toString() {
+            return "Page{" +
+                    "itemList=" + itemList +
+                    '}';
+        }
+    }
+    public class Item{
+        private String name;
+        private boolean isSelect;
+
+        public boolean isSelect() {
+            return isSelect;
+        }
+
+        public void setSelect(boolean select) {
+            isSelect = select;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Item(String name, boolean isSelect) {
+            this.name = name;
+            this.isSelect = isSelect;
+        }
+
+        @Override
+        public String toString() {
+            return "Item{" +
+                    "name='" + name + '\'' +
+                    '}';
         }
     }
 }
