db7b2e2b4313aa1f675f696fbc34efbf5b2b06bf
mirse-partner <879937147@qq.com>
2020-09-06 21:34:21 +0800

    使用模块组件化

---
+++ module_customview/src/main/java/com/wdz/module_customview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wdz.module_customview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.module_customview_activity_main);
+    }
+}
