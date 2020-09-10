7d21a4681aa605f3b3c703580271f8ae454ad291
mirse-partner <879937147@qq.com>
2020-09-10 22:36:11 +0800

    添加架构、通信module

---
+++ module_architecture/src/main/java/com/wdz/module_architecture/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wdz.module_architecture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.module_architecture_activity_main);
+    }
+}
---
+++ module_communication/src/main/java/com/wdz/module_communication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wdz.module_communication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.module_communication_activity_main);
+    }
+}
